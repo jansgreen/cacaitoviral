a8b7d2192849b5f3e8220fff0d0f6aa641e24a69
Luis Pena <Luis Pena>
2020-12-31 16:55:14 -0500

    areglando los archivos static 12

--- jansviral/settings.py
+++ jansviral/settings.py
@@ -148,11 +148,15 @@ MENU_URL = '/accounts/menu/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    "jansviral/static",
+
+STATIC_ROOT = os.path.join(BASE_DIR,"jansviral")
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [ 
+os.path.join(BASE_DIR, "static"),
 ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
