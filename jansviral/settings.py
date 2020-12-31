d74654197fe15e4035108c4c29570924ec92a387
Luis Pena <Luis Pena>
2020-12-31 17:54:47 -0500

    areglando los archivos static 14

--- jansviral/settings.py
+++ jansviral/settings.py
@@ -150,12 +150,9 @@ MENU_URL = '/accounts/menu/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR,"jansviral")
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [ 
-os.path.join(BASE_DIR, "static"),
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
