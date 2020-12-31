581d44748e00d5c16c551a8bbda9ebf76a8d0a03
Luis Pena <Luis Pena>
2020-12-31 18:25:02 -0500

    areglando los archivos static 20

--- jansviral/settings.py
+++ jansviral/settings.py
@@ -150,8 +150,8 @@ MENU_URL = '/accounts/menu/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
